5df69849f41f0045b0c679385892bf91830af62d
Corentin0003 <maurerco@outlook.com>
2023-12-21 10:37:25 +0100

    Modifications

--- script.js
+++ script.js
@@ -12,43 +12,43 @@
   const content = [
     {
         "title": "Projet 1",
-        "year": "2023",
-        "domain": "Typo"
+        "domain": "Typo",
+        "year": "2023"
     },
     {
         "title": "Projet 2",
-        "year": "2024",
-        "domain": "Graphisme"
+        "domain": "Graphisme",
+        "year": "2024"
     },
     {
         "title": "Projet 3",
-        "year": "2025",
-        "domain": "Photographie"
+        "domain": "Photographie",
+        "year": "2025"
     },
     {
         "title": "Projet 3",
-        "year": "2025",
-        "domain": "Photographie"
+        "domain": "Photographie",
+        "year": "2025"
     },
     {
         "title": "Projet 3",
-        "year": "2025",
-        "domain": "Photographie"
+        "domain": "Photographie",
+        "year": "2025"
     },
     {
         "title": "Projet 3",
+        "domain": "Photographie",
         "year": "2025",
-        "domain": "Photographie"
     },
     {
         "title": "Projet 3",
+        "domain": "Photographie",
         "year": "2025",
-        "domain": "Photographie"
     },
     {
         "title": "Projet 3",
+        "domain": "Photographie",
         "year": "2025",
-        "domain": "Photographie"
     },
 ]
 const table = document.getElementById("table");
@@ -56,20 +56,21 @@ for (element of content) {
     const tr = document.createElement("tr");
 
     const tdTitle = document.createElement("td");
-    const tdYear = document.createElement("td");
     const tdDomain = document.createElement("td");
+    const tdYear = document.createElement("td");
 
     tdTitle.classList.add("col-2")
     tdYear.classList.add("col-2")
     tdDomain.classList.add("col-2")
 
     tdTitle.innerHTML = element.title;
-    tdYear.innerHTML = element.year;
     tdDomain.innerHTML = element.domain;
-    
-    tr.appendChild(tdTitle);
+    tdYear.innerHTML = element.year;
+
     tr.appendChild(tdYear);
+    tr.appendChild(tdTitle);
     tr.appendChild(tdDomain);
+   
     table.appendChild(tr);
 
 }
